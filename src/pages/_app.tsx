f9b5932cef814fc5d93dac69d3613266141e811f
Kaushik Asp <aspkaushik@gmail.com>
2024-05-26 15:16:57 +0530

    [fix] Fixes types and builds

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
-import { GeistSans } from "geist/font/sans";
 import { type AppType } from "next/app";
 import { SessionProvider } from "next-auth/react"
-
+import { type Session } from 'next-auth';
 
 import "~/styles/globals.css";
 
-const MyApp: AppType = ({ Component, pageProps: { session, ...pageProps } }) => {
+interface MyAppProps {
+  session: Session | null;
+}
+
+const MyApp: AppType<MyAppProps> = ({ Component, pageProps: { session, ...pageProps } }) => {
   return (
     <SessionProvider session={session}>
       <main>
